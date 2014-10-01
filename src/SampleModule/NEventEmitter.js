85afb1121a59fa7ac5379501a671d4ccb7fc5de7
Shahm Nattarshah <n.shahm@gmail.com>
2014-10-01 17:17:27 -0400

    Added Event Emitter class

---
+++ src/SampleModule/NEventEmitter.js
@@ -0,0 +1,44 @@
+var util = require('util');
+var EE = require('events').EventEmitter;
+
+
+var CompareNumbers = function() {};
+
+console.log(CompareNumbers);
+util.inherits(CompareNumbers, EE);
+console.log(CompareNumbers);
+
+CompareNumbers.prototype.checkGreater = function (a, b) {
+	if (a < b) {
+		this.emit('isgreater', true);
+	} else {
+		this.emit('error', new Error ('A is less than B'));
+	}
+};
+
+//var CompareNumbers = function() {
+//	
+//	return {
+//		checkGreater : function (a, b) {
+//			if (a < b) {
+//				this.emit('isgreater', true);
+//			} else {
+//				this.emit('error', new Error ('A is less than B'));
+//			}		
+//		}
+//	};
+//};
+
+
+var compareNumberInstance = new CompareNumbers();
+
+compareNumberInstance.on('isgreater', function (flag) {
+	console.log('isgreater event triggered', flag);
+});
+
+compareNumberInstance.on('error', function (error) {
+	console.log(error);
+});
+
+compareNumberInstance.checkGreater(1, 2);
+
--- src/SampleModule/index.js
+++ src/SampleModule/index.js
@@ -1,3 +1,4 @@
 // Importing module can be used with .js by default. But actually not required.
 var nbuffer = require('./NBuffer.js')
 var nfile = require('./NFile')
+var nfile = require('./NEventEmitter')
